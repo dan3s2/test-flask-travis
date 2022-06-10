473ca5f04680e45e582c67ec21c0cc1829e7410e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-06-10 18:39:25 +0000

    Deploy dan3s2/test-flask-travis to github.com/dan3s2/test-flask-travis.git:gh-pages

--- src/test_woreda.py
+++ src/test_woreda.py
@@ -19,7 +19,7 @@ class TestAgroadisory(unittest.TestCase):
 
         response = self.app.get('http://127.0.0.1:5000/woredas/woreda1',
                                 headers={"Content-Type": "application/json"})
-        #print(response)
+        # print(response)
         #self.assertEqual(str, type(response.json['Woreda']))
         self.assertEqual(200, response.status_code)
 
